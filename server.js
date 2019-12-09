2fef126724c44f45c52913f8b4ffdc85b5c59ec9
Bradley Gore <gorebr@flip3.engr.oregonstate.edu>
2019-12-08 17:18:09 -0800

    Added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+var port = 3202;
+
+
+app.listen(port, function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("== Server listening on port", port);
+  });
