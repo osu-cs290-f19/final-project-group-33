98be2b500e900101c0a4c822da6fc383da088e37
Bradley Gore <gorebr@flip2.engr.oregonstate.edu>
2019-12-09 17:14:13 -0800

    fixed git error

--- public/index.js
+++ public/index.js
@@ -17,20 +17,7 @@ function insertNewPost(name, license, car, time, date, start, end, isDriver) {
   var postsContainer = document.getElementById('posts-container');
   postsContainer.insertAdjacentHTML('beforeend', postHTML);
   // return postHTML;
-<<<<<<< HEAD
 }
 
 var createButton = document.getElementById('create');
 createButton.addEventListener('click', insertNewPost);
-=======
-  addPostToJSON(name, license, car, time, date, start, end, isDriver);
-  }
-
-function addPostToJSON(name, license, car, time, date, start, end, isDriver){
-  var post = {"name": name, "license": license, "car": car, "time": time, "date": date, "start": start, "end": end, "isDriver": isDriver};
-  fs.appendFile("postData.json", post, function (err) {
-  if (err) throw err;
-  console.log('Updated!');
-});
-}
->>>>>>> d23bc5dcb4d2250a624f5f397b2b4a4bb1e04fa7
