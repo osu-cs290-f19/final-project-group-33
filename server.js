caaa392cb6023847c7dd2ca1c65f363b5835b5e2
Melvin Ma <mamel@flip3.engr.oregonstate.edu>
2019-12-09 19:20:58 -0800

    added server file wrtie functianolity

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 var postData = require('./postData.json');
+var bodyParse = require('body-parser');
 
 var app = express();
 var port = 3712;
@@ -10,6 +11,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
+app.use(bodyParse.json());
 
 
 app.get('*', function (req, res, next){
@@ -41,7 +43,34 @@ app.get('/:fileName', function (req, res, next){
 
 
 });
+app.post('/driver/addPost', function(req, res, next){
+    if (req.body && req.body.name && req.body.license && req.body.car && req.body.time && req.body.date && req.body.start && req.body.end) {
+      postData.push({
+        name: req.body.name,
+        license: req.body.license,
+        car: req.body.car,
+        time: req.body.time,
+        date: req.body.date,
+        start: req.body.start,
+        req.body.end
+      });
+      console.log("== postData:", postData);
+      fs.writeFile(
+        __dirname + '/postData.json',
+        JSON.stringify(postData, 2, null),
+        function (err) {
+          if (!err) {
+            res.status(200).send();
+          } else {
+            res.status(500).send("Failed to write data on server side.");
+          }
+        }
+      );
+    } else {
+      res.status(400).send("Request body is missing values.");
+    }
 
+})
 app.get('*', function (req, res, next){
   console.log("404");
   res.status(404).render('404', {path : req.url});
