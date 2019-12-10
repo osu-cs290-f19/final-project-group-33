d23bc5dcb4d2250a624f5f397b2b4a4bb1e04fa7
Melvin Ma <mamel@flip2.engr.oregonstate.edu>
2019-12-09 17:05:24 -0800

    added post data.json and added function in js to write json object

--- public/index.js
+++ public/index.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 function insertNewPost(name, license, car, time, date, start, end, isDriver) {
   var postHTML = Handlebars.templates.postTemplate({
     name: name,
@@ -14,4 +16,13 @@ function insertNewPost(name, license, car, time, date, start, end, isDriver) {
   var postsContainer = document.getElementById('posts-container');
   postsContainer.insertAdjacentHTML('beforeend', postHTML);
   // return postHTML;
+  addPostToJSON(name, license, car, time, date, start, end, isDriver);
   }
+
+function addPostToJSON(name, license, car, time, date, start, end, isDriver){
+  var post = {"name": name, "license": license, "car": car, "time": time, "date": date, "start": start, "end": end, "isDriver": isDriver};
+  fs.appendFile("postData.json", post, function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+});
+}
